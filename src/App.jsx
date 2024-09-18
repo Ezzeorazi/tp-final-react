480cd27ad15ca27e7fec7d3f6f2e788ff63b2a75
Ezzeorazi <ezequiel.orazi90@gmail.com>
2024-09-17 22:09:26 -0300

    Ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import AppRouter from "./routers/AppRouter";
-import { Provider } from "react-redux";
 import "./styles/global.css";
 import "./styles/variables.css";
 
 function App() {
   return (
-    <Provider store={store}>
       <AppRouter />
-    </Provider>
   );
 }
 
